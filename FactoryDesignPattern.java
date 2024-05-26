26af9e119b72de7febe86be46f7373631a1e93fa
Priyanka-droid <123priyankarai@gmail.com>
2024-05-26 16:20:03 +0530

    1. Reformat FactoryDesignPattern 2. Close scanner object after use

--- FactoryDesignPattern.java
+++ FactoryDesignPattern.java
@@ -2,9 +2,8 @@ import java.util.Scanner;
 
 public class FactoryDesignPattern {
     public static void main(String[] args) {
-        Logistics l= new Logistics();
         try {
-            l.dealWithLogistic();
+            Logistics.dealWithLogistic();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -13,21 +12,22 @@ public class FactoryDesignPattern {
 }
 
 class Logistics {
-    void dealWithLogistic() throws Exception{
-        Scanner sc= new Scanner(System.in);
-        String transportMode= sc.nextLine();
+    static void dealWithLogistic() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String transportMode = sc.nextLine();
+        sc.close();
 
         TransportLogistic t;
-        switch(transportMode){
-            case "ship":{
-                t= new ShipLogisticc();
+        switch (transportMode) {
+            case "ship": {
+                t = new ShipLogisticc();
             }
-            break;
-            case "truck":{
-                t= new TruckLogistic();
+                break;
+            case "truck": {
+                t = new TruckLogistic();
             }
-            break;
-            default:{
+                break;
+            default: {
                 throw new Exception("go deliver it on your back!!!!");
             }
         }
@@ -37,60 +37,64 @@ class Logistics {
     }
 }
 
-//creator class
-abstract class TransportLogistic{
+// creator class
+abstract class TransportLogistic {
 
-    int findDeliveryPrice(){
-        Mode m=createLogistic();
+    int findDeliveryPrice() {
+        Mode m = createLogistic();
         return m.deliverPrice();
     }
-    void showDeliveryMessage(){
-        Mode m=createLogistic();
+
+    void showDeliveryMessage() {
+        Mode m = createLogistic();
         m.deliverMessage();
     }
 
-    //factory method
-    abstract  Mode createLogistic();
+    // factory method
+    abstract Mode createLogistic();
 }
 
-//concrete CreatorA
-class TruckLogistic extends TransportLogistic{
-     Mode createLogistic(){
+// concrete CreatorA
+class TruckLogistic extends TransportLogistic {
+    Mode createLogistic() {
         return new Truck();
     }
 }
-//concrete creatorB
-class ShipLogisticc extends TransportLogistic{
-     Mode createLogistic(){ 
+
+// concrete creatorB
+class ShipLogisticc extends TransportLogistic {
+    Mode createLogistic() {
         return new Ship();
     }
 }
 
-
-//product interface
-interface Mode{
+// product interface
+interface Mode {
     public void deliverMessage();
+
     public int deliverPrice();
 
 }
 
-//concrete product A
-class Ship implements Mode{
-    public void deliverMessage(){
+// concrete product A
+class Ship implements Mode {
+    public void deliverMessage() {
         System.out.println("deliver with ship");
     }
-    public int deliverPrice(){
+
+    public int deliverPrice() {
         return 10;
     }
 
 }
 
-//concrete product B
-class Truck implements Mode{
-    public void deliverMessage(){
+// concrete product B
+class Truck implements Mode {
+    public void deliverMessage() {
         System.out.println("deliver with truck");
     }
-    public int deliverPrice(){
+
+    public int deliverPrice() {
         return 20;
     }
 
--- NonFactoryDesignPattern.java
+++ NonFactoryDesignPattern.java
@@ -1,53 +1,52 @@
 import java.util.Scanner;
 
-public class NonFactoryDesignPattern{
+public class NonFactoryDesignPattern {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in); 
-        String inputLogisticMode= sc.nextLine();
-        switch(inputLogisticMode){
-            case "ship":{
-                ShipTemp ship=new ShipTemp();
+        Scanner sc = new Scanner(System.in);
+        String inputLogisticMode = sc.nextLine();
+        switch (inputLogisticMode) {
+            case "ship": {
+                ShipTemp ship = new ShipTemp();
                 ship.deliver();
-                int deliveryCost= ship.shipDeliveryCost();
+                int deliveryCost = ship.shipDeliveryCost();
                 System.out.println(deliveryCost);
             }
-            break;
-            case "truck":{
-                TruckTemp truck= new TruckTemp();
+                break;
+            case "truck": {
+                TruckTemp truck = new TruckTemp();
                 truck.deliver();
-                int deliveryCost= truck.truckDeliveryCost();
+                int deliveryCost = truck.truckDeliveryCost();
                 System.out.println(deliveryCost);
             }
-            break;
-           default:{
-                System.out.println("What the hell do you want to deliver with!!!"); 
+                break;
+            default: {
+                System.out.println("What the hell do you want to deliver with!!!");
             }
         }
 
-
-
-        //adding another logistics requires making changes in this class. too lazy for that. Better way to do this=> check for factory pattern class
+        // adding another logistics requires making changes in this class. too lazy for
+        // that. Better way to do this=> check for factory pattern class
 
     }
 }
 
-
-class ShipTemp{
-    void deliver(){
+class ShipTemp {
+    void deliver() {
         System.out.println("deliver with ship");
     }
-    int shipDeliveryCost(){
+
+    int shipDeliveryCost() {
         return 10;
     }
 }
 
-class TruckTemp{
-    void deliver(){
+class TruckTemp {
+    void deliver() {
         System.out.println("deliver with truck");
     }
 
-    int truckDeliveryCost(){
+    int truckDeliveryCost() {
         return 20;
     }
 }
